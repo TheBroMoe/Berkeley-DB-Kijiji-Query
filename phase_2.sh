f0e9894dfb5d6fca70a0119466cc3d1d000e5781
WesCamelCase <wespeterson2@gmail.com>
2018-11-24 16:08:23 -0700

    g

--- phase_2.sh
+++ phase_2.sh
@@ -3,5 +3,3 @@ sort -u 1k-ads.txt -o 1k-ads.txt |cat 1k-ads.txt | ./break.pl | db_load -T -t ha
 sort -u 1k-terms.txt -o 1k-terms.txt | cat 1k-terms.txt | ./break.pl | db_load -T -t btree te.idx
 sort -u 1k-pdates.txt -o 1k-pdates.txt | cat 1k-pdates.txt | ./break.pl | db_load -T -t btree da.idx
 sort -u 1k-prices.txt -o 1k-prices.txt | cat 1k-prices.txt | ./break.pl | db_load -T -t btree pr.idx
-
-
