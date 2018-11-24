7b0e21a7159f29fd72153b9a9df040c429835a21
TheBroMoe <kebbi@ualberta.ca>
2018-11-24 13:18:05 -0700

    update phase 2 file

--- phase_2.sh
+++ phase_2.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-
-sort -u 1k-ads.txt -o 1k-ads.txt
-sort -u 1k-terms.txt -o 1k-terms.txt
-sort -u 1k-pdates.txt -o 1k-pdates.txt
-sort -u 1k-prices.txt -o 1k-prices.txt
-
-cat 1k-ads.txt | ./break.pl | db_load -T -t hash ad.idx
-cat 1k-terms.txt | ./break.pl | db_load -T -t btree te.idx
-cat 1k-pdates.txt | ./break.pl | db_load -T -t btree da.idx
-cat 1k-prices.txt | ./break.pl | db_load -T -t btree pr.idx
+sort -u 1k-ads.txt -o 1k-ads.txt |cat 1k-ads.txt | ./break.pl | db_load -T -t hash ad.idx
+sort -u 1k-terms.txt -o 1k-terms.txt | cat 1k-terms.txt | ./break.pl | db_load -T -t btree te.idx
+sort -u 1k-pdates.txt -o 1k-pdates.txt | cat 1k-pdates.txt | ./break.pl | db_load -T -t btree da.idx
+sort -u 1k-prices.txt -o 1k-prices.txt | cat 1k-prices.txt | ./break.pl | db_load -T -t btree pr.idx
 
 
