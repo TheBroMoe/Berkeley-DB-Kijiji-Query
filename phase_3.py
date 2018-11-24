7b9146dea9852469412c9afa8a36ef71d5b78dc0
TheBroMoe <kebbi@ualberta.ca>
2018-11-24 14:54:12 -0700

    add phase_3.py

---
+++ phase_3.py
@@ -0,0 +1,55 @@
+'''
+alphanumeric    ::= [0-9a-zA-Z_-]
+numeric		::= [0-9]
+date            ::= numeric numeric numeric numeric '/' numeric numeric '/' numeric numeric
+datePrefix      ::= 'date' whitespace* ('=' | '>' | '<' | '>=' | '<=')
+dateQuery       ::= datePrefix whitespace* date
+price		::= numeric+
+pricePrefix     ::= 'price' whitespace* ('=' | '>' | '<' | '>=' | '<=')
+priceQuery	::= pricePrefix whitespace* price
+location	::= alphanumeric+
+locationPrefix  ::= 'location' whitespace* '='
+locationQuery	::= locationPrefix whitespace* location
+cat		::= alphanumeric+
+catPrefix  	::= 'cat' whitespace* '='
+catQuery	::= catPrefix whitespace* cat
+term            ::= alphanumeric+
+termSuffix      ::= '%' 
+termQuery       ::= term | term termSuffix
+expression      ::= dateQuery | priceQuery | locationQuery | catQuery | termQuery 
+query           ::= expression (whitespace expression)*
+
+'''
+
+import re
+
+alphanumeric = "[0-9a-zA-Z_-]"
+numeric = "[0-9]"
+date = "(" + numeric * 4 + "/" + numeric * 2 + "/" + numeric * 2 + ")"
+datePrefix = "date\s*(=|>|<|>=|<=)"
+dateQuery = datePrefix + "\s*" + date
+price = numeric + "+"
+pricePrefix = 'price\s*(=|>|<|>=|<=)'
+priceQuery = pricePrefix + "\s*" + price
+location = alphanumeric + '+'
+locationPrefix = 'location\s*='
+locationQuery = locationPrefix + '\s*' + location
+cat = alphanumeric + "+"
+catPrefix = 'cat\s\='
+catQuery = catPrefix + "\s*="
+term = alphanumeric + "+"
+termSuffix = '%'
+termQuery = term + "|" + term + termSuffix
+expression = dateQuery + "|" + priceQuery + "|" + locationQuery + "|" + catQuery + "|" + termQuery
+query = expression + "(\s*" + expression + ")*"
+
+
+
+
+
+def main():
+    print('hello')
+
+
+if __name__ == "__main__":
+    main()
